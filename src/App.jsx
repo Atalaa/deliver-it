be10aa7abfbdc6025f4d1d673803d87d99970e86
Atala <atala.rchidi@gmail.com>
2020-06-02 15:53:18 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import logo from './images/delivery.jpg'
+
+
+function App(){
+    return(
+        <div>
+            <img src={logo} alt="logo-car"/>
+            <header>Deliver it!</header>
+        </div>
+
+    )
+}
+
+export default App;
